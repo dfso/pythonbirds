52d211b385c0fea0c1000217a6ad1ee1ee7204a1
dfso <denison.new@gmail.com>
2018-08-21 19:14:02 -0400

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,22 @@
 class Pessoa():
 
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá, {id(self)}'
 
 
 if __name__ == "__main__":
-    p = Pessoa('Dênison')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    p.nome = 'Jhonny'
-    print(p.nome)
-    print(p.idade)
+    joao = Pessoa(nome='João Victor', idade=15)
+    denison = Pessoa(joao, nome='Dênison Fábio', idade=36)
+    print(Pessoa.cumprimentar(denison))
+    print(id(denison))
+    print(denison.cumprimentar())
+    print(denison.nome)
+    print(denison.idade)
+
+    for filho in denison.filhos:
+        print(f'nome do filho: {filho.nome}')
